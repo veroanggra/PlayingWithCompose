ddb61b8b243e92268e311bd44cd42bf8a300c6c7
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-20 21:55:53 +0700

    finishing compose view

--- app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
+++ app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
@@ -2,11 +2,39 @@ package com.veroanggra.migratingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import com.veroanggra.migratingsample.databinding.ActivityMainBinding
+import com.veroanggra.migratingsample.theme.White
+import com.veroanggra.migratingsample.widget.InputTextField
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        mainBinding.inputText.setContent {
+            Column(modifier = Modifier) {
+                InputTextField(
+                    modifier = Modifier,
+                    hint = "Username",
+                    type = KeyboardType.Text,
+                    capitalization = KeyboardCapitalization.None,
+                    color = White
+                )
+                InputTextField(
+                    modifier = Modifier,
+                    hint = "Enter Password",
+                    type = KeyboardType.Password,
+                    capitalization = KeyboardCapitalization.None,
+                    color = White
+                )
 
+            }
+
+        }
     }
 }
