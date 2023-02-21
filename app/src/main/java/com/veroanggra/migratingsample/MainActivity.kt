7537e7aa7426d524109547867a09ac829798f727
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-22 02:25:48 +0700

    add row

--- app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
+++ app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
@@ -3,18 +3,36 @@ package com.veroanggra.migratingsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.veroanggra.composesubmission.ui.theme.PlayingWithComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PlayingWithComposeTheme{
+            PlayingWithComposeTheme {
                 Surface() {
+                    RowArrange(modifier = Modifier)
 
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RowArrange(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Row1")
+        Text(text = "Row2")
+        Text(text = "Row3")
+        Text(text = "Row4")
+        Text(text = "Row5")
     }
 }
