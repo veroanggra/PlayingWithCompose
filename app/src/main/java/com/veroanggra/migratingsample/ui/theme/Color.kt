f32d7c6b7ee1aa41515c9a7396973c38b79a73a9
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-22 01:55:09 +0700

    initial code

---
+++ app/src/main/java/com/veroanggra/migratingsample/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.veroanggra.composesubmission.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Cream = Color(0xFFF2EFEA)
+val DarkCream = Color(0xFFEAE4D8)
+val Green = Color(0xFF92B1A9)
+val DarkGreen = Color(0xFF5B8078)
+val NeonGreen = Color(0xFFBBE2DD)
+val GreenPickerColor = Color(0xFF3B7672)
+val YellowPickerColor = Color(0xFFEAC776)
