5b9c37c51a2a7e157e1142c9d0f19e32342504bd
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-03-05 21:18:49 +0700

    add guideline

--- app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
+++ app/src/main/java/com/veroanggra/migratingsample/MainActivity.kt
@@ -7,19 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
-import com.veroanggra.composesubmission.ui.theme.DarkGreen
-import com.veroanggra.composesubmission.ui.theme.PlayingWithComposeTheme
+import com.veroanggra.composesubmission.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +36,13 @@ fun ConstraintScreen() {
     val constraint = ConstraintSet {
         val firstRect = createRefFor("firstRect")
         val secondRect = createRefFor("secondRect")
+        val thirdRect = createRefFor("thirdRect")
+        val fourthRect = createRefFor("fourthRect")
+        val fifthRect = createRefFor("fifthRect")
+        val sixRect = createRefFor("sixRect")
+        val sevenRect = createRefFor("sixRect")
+        val guideline = createGuidelineFromBottom(0.1f)
+
 
         constrain(firstRect) {
             top.linkTo(parent.top)
@@ -47,15 +51,69 @@ fun ConstraintScreen() {
             width = Dimension.fillToConstraints
             height = Dimension.value(100.dp)
         }
-        constrain(secondRect){
-
+        constrain(secondRect) {
+            top.linkTo(firstRect.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            width = Dimension.fillToConstraints
+            height = Dimension.value(100.dp)
         }
+        constrain(secondRect) {
+            top.linkTo(firstRect.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            width = Dimension.percent(1f)
+            height = Dimension.value(100.dp)
+        }
+        constrain(thirdRect) {
+            top.linkTo(firstRect.bottom)
+            start.linkTo(secondRect.end)
+            end.linkTo(parent.end)
+            width = Dimension.percent(1f)
+            height = Dimension.value(100.dp)
+        }
+        constrain(fourthRect) {
+            top.linkTo(secondRect.bottom)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        constrain(fifthRect) {
+            top.linkTo(secondRect.bottom)
+            start.linkTo(fourthRect.end)
+            end.linkTo(parent.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+//        createHorizontalChain(fourthRect, fifthRect, chainStyle = ChainStyle.Spread)
+//        createHorizontalChain(fourthRect, fifthRect, chainStyle = ChainStyle.SpreadInside)
+        createHorizontalChain(fourthRect, fifthRect, chainStyle = ChainStyle.Packed)
     }
     ConstraintLayout(constraintSet = constraint, modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
                 .background(DarkGreen)
                 .layoutId("firstRect")
+        )
+        Box(
+            modifier = Modifier
+                .layoutId("secondRect")
+                .background(Cream)
+        )
+        Box(
+            modifier = Modifier
+                .layoutId("thirdRect")
+                .background(YellowPickerColor)
+        )
+        Box(
+            modifier = Modifier
+                .layoutId("fourthRect")
+                .background(GreenPickerColor)
+        )
+        Box(
+            modifier = Modifier
+                .layoutId("fifthRect")
+                .background(Purple40)
         )
     }
 }
